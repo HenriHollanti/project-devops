31ac9bb4f39516b3b3fc6252acc1559420f74e15
Henri Hollanti <henri.hollanti@hotmail.com>
2026-03-05 17:22:09 +0200

    fix(Logic): modify checkWinner() return type to be boolean

--- app/src/main/java/tictactoe/Logic.java
+++ app/src/main/java/tictactoe/Logic.java
@@ -4,7 +4,7 @@ public interface Logic {
     /**
      * A method for checking the game's winner.
      */
-    void checkWinner();
+    boolean checkWinner();
     /**
      * A method for resetting the board or the
      * game's state.
