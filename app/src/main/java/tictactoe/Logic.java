a773fbd6d601a603c3b34a3976a9748337c6ae6b
Henri Hollanti <henri.hollanti@hotmail.com>
2026-03-05 15:50:38 +0200

    feat(javadoc): add javadoc for resetBoard in Logic

--- app/src/main/java/tictactoe/Logic.java
+++ app/src/main/java/tictactoe/Logic.java
@@ -5,5 +5,9 @@ public interface Logic {
      * A method for checking the game's winner.
      */
     void checkWinner();
+    /**
+     * A method for resetting the board or the
+     * game's state.
+     */
     void resetBoard();
 }
